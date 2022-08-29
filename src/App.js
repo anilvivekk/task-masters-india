9002766fa349d62e3d08203acea6cc9bb77018fc
anilvivekk <avanilvivek056@gmail.com>
2022-08-29 16:59:07 +0530

    snippets

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [snippet, setSnippet] = useState([]);
+  const [itemToSearch, SetItemToSearch] = useState('');
+  const [searchedResult, setSearchResult] = useState(null);
+
+  useEffect(()=>{
+    const BASE_URL = 'https://mocki.io/v1/f9e16211-d239-48a4-a1dd-4de8cf4bfa15';
+    
+    const fetchData = async ()=>{
+      try {
+        const response = await fetch(BASE_URL);
+        const json = await response.json();
+        // console.log(json);
+        setSnippet(json.results);
+        // console.log(snippet);
+        setSearchResult(json.results);
+      }catch(err){
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const filterOut = (e)=>{
+    const keyWords = (e.target.value);
+
+    if(keyWords !== ''){
+      const results = searchedResult.filter((item)=> {
+        return item.title.toLowerCase().startsWith(keyWords.toLowerCase());
+      });
+      setSearchResult(results);
+    } else {
+      setSearchResult(snippet);
+    }
+    SetItemToSearch(keyWords);
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <form >
+    <input type='search' value={itemToSearch} placeholder='search here' onChange={filterOut} />
+    </form>
+    
+    <div>
+      {searchedResult && searchedResult.length > 0 ? (
+        searchedResult.map((item)=>(
+          <li>{item.title} {`-->`}{item.subtitle}</li>
+        ))
+      ):(<h2 style={{color:'red'}}>No results found</h2>)}
     </div>
+    </>
   );
 }
 
